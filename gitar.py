71c7ae0a2d3dfc9c04a8f68113b4ef1f9505de79
Felix Schill <felix.schill@hydromea.com>
2018-02-28 14:58:06 +0100

    added labels to editor windows, added support for '.' as branch argument for local working copy, and some bug fixes

--- gitar.py
+++ gitar.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import *
 from PyQt5.Qsci import *
 
 import difflib
-import os, sys, subprocess
+import os, sys, subprocess,  os.path
 
 def aligner(lines1, lines2):
     diffs = difflib._mdiff(lines1, lines2)
@@ -15,11 +15,11 @@ def aligner(lines1, lines2):
     for fromdata, todata, flag in diffs:
         try:
             # store HTML markup of the lines into the lists
-            if todata[1].startswith('\0+'):
+            if todata[1].startswith('\0+') and fromdata[1].strip()=="":
                 fromlist.append(None)
             else:
                 fromlist.append(fromdata[1])
-            if fromdata[1].startswith('\0-'):
+            if fromdata[1].startswith('\0-') and todata[1].strip()=="":
                 tolist.append(None)
             else:
                 tolist.append(todata[1])
@@ -48,10 +48,10 @@ class cd:
 def getFromGit(path_to_repository, branchname, filepath):
     lines=""
     try:
-        if branchname == "":
-            print(path_to_repository+filepath)
+        if branchname == "" or branchname==".":
+            #print(path_to_repository+filepath)
             file = open((path_to_repository+filepath).strip())
-            print("file opened")
+            #print("file opened")
             lines = file.readlines()
             return [l.rstrip()+"\n" for l in lines]
         else:
@@ -59,7 +59,7 @@ def getFromGit(path_to_repository, branchname, filepath):
                 lines = subprocess.check_output('git show %s:%s'%(branchname, filepath), shell=True).decode('utf-8').splitlines()
             return [l.rstrip()+"\n" for l in  lines]
     except:
-        print("file error")
+        #print("file error")
         return ""
 
 def getChangedFilesFromGit(path_to_repository, branch1, branch2):
@@ -71,6 +71,10 @@ def getChangedFilesFromGit(path_to_repository, branch1, branch2):
             elif branch1 == "":
                     lines = subprocess.check_output('git diff --name-only %s'%branch2, shell=True).decode('utf-8').splitlines()
             else:
+                if branch1==".":
+                    branch1  = getGitCurrentBranch()
+                if branch2==".":
+                    branch2  = getGitCurrentBranch()
                 lines = subprocess.check_output('git diff --name-only %s...%s' % (branch1, branch2), shell=True).decode(
                     'utf-8').splitlines()
         return [l+"\n" for l in  lines]
@@ -95,6 +99,60 @@ def getGitCurrentBranch():
         return ""
 
 
+class EditorWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.label = QLabel()
+        self.editor = QsciScintilla()
+        self.configureEditor(self.editor)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.editor)
+        
+    def configureEditor(self, editor):
+        self.__lexer = QsciLexerCPP()
+        editor.setLexer(self.__lexer)
+        editor.setMarginType(1, QsciScintilla.TextMargin)
+        editor.setMarginType(0, QsciScintilla.SymbolMargin)
+        editor.setMarginMarkerMask(1, 0b1111)
+        editor.setMarginMarkerMask(0, 0b1111)
+        editor.setMarginsForegroundColor(QColor("#ffFF8888"))
+        editor.setUtf8(True)  # Set encoding to UTF-8
+        #editor.indicatorDefine(QsciScintilla.FullBoxIndicator, 0)
+        editor.indicatorDefine(QsciScintilla.BoxIndicator, 0)
+        editor.setAnnotationDisplay(QsciScintilla.AnnotationStandard)
+
+    def updateText(self, text,  label=""):
+        marginTextStyle= QsciStyle()
+        marginTextStyle.setPaper(QColor("#ffFF8888"))
+        self.editor.setText("")
+        self.label.setText(label)
+        skipped_lines = 0
+        annotation=None
+        for linenumber, l in enumerate(text):
+            idx=linenumber-skipped_lines
+            if l is None:
+                #editor.append("\n")
+                if annotation is None:
+                    annotation="<"
+                else:
+                    annotation+="\n<"
+                skipped_lines+=1
+                self.editor.setMarginText(idx, "~", marginTextStyle)
+            else:
+                if annotation is not None:
+                    self.editor.annotate(idx-1, annotation, 0)
+                    annotation=None
+                if '\0' in l or '\1' in l:
+                    self.editor.append(l.replace('\0+', '').replace('\0-', '').replace('\m', '').replace('\0^', '').replace('\1', ''))
+                    self.editor.markerAdd(idx, QsciScintilla.Circle)
+                    self.editor.setMarginText(idx, l[1], marginTextStyle)
+                    self.editor.fillIndicatorRange(idx, l.find('\0'), idx, l.rfind("\1"), 0)
+                else:
+                    self.editor.append(l)
+
+        
 class CustomMainWindow(QMainWindow):
 
     def __init__(self):
@@ -153,21 +211,25 @@ class CustomMainWindow(QMainWindow):
 
         self.addDockWidget(Qt.LeftDockWidgetArea, self.file_view)
 
+        ignore_list=["hex"]
         for f in self.files:
-            size=self.calcDiffSize(f)
-            if size>0:
+            # if comparing to working copy, only show files that exist locally
+            exist=True
+            size=0
+            if self.branch1=="" or self.branch2=="":
+                exist = os.path.isfile(self.gitpath+f.strip())
+            if exist and not (f.strip().split(".")[-1]  in ignore_list):
+                size=self.calcDiffSize(f)
+            if exist and size>0:
                 self.file_list.addItem(f.strip()+": (%i)"%size)
 
         # QScintilla editor setup
         # ------------------------
 
         # ! Make instance of QsciScintilla class!
-        self.left_editor = QsciScintilla()
-        self.configureEditor(self.left_editor)
+        self.left_editor = EditorWidget()
 
-        self.right_editor = QsciScintilla()
-        self.configureEditor(self.right_editor)
-
+        self.right_editor = EditorWidget()
 
         self.comparison_area.addWidget(self.left_editor)
         self.comparison_area.addWidget(self.right_editor)
@@ -177,7 +239,7 @@ class CustomMainWindow(QMainWindow):
 
         self.show()
 
-        self.left_editor.verticalScrollBar().valueChanged.connect( self.right_editor.verticalScrollBar().setValue)
+        self.left_editor.editor.verticalScrollBar().valueChanged.connect( self.right_editor.editor.verticalScrollBar().setValue)
 
     ''''''
 
@@ -196,49 +258,9 @@ class CustomMainWindow(QMainWindow):
         lines2 = getFromGit(self.gitpath, self.branch2, self.filepath)
 
         left, right, flags = aligner(lines1, lines2)
-        self.updateText(self.left_editor, left)
-        self.updateText(self.right_editor, right)
+        self.left_editor.updateText( left,  self.branch1+" : "+self.filepath)
+        self.right_editor.updateText(right, self.branch2+" : "+self.filepath)
 
-    def configureEditor(self, editor):
-        self.__lexer = QsciLexerCPP()
-        editor.setLexer(self.__lexer)
-        editor.setMarginType(1, QsciScintilla.TextMargin)
-        editor.setMarginType(0, QsciScintilla.SymbolMargin)
-        editor.setMarginMarkerMask(1, 0b1111)
-        editor.setMarginMarkerMask(0, 0b1111)
-        editor.setMarginsForegroundColor(QColor("#ffFF8888"))
-        editor.setUtf8(True)  # Set encoding to UTF-8
-        #editor.indicatorDefine(QsciScintilla.FullBoxIndicator, 0)
-        editor.indicatorDefine(QsciScintilla.BoxIndicator, 0)
-        editor.setAnnotationDisplay(QsciScintilla.AnnotationStandard)
-
-    def updateText(self, editor, text):
-        marginTextStyle= QsciStyle()
-        marginTextStyle.setPaper(QColor("#ffFF8888"))
-        editor.setText("")
-        skipped_lines = 0
-        annotation=None
-        for linenumber, l in enumerate(text):
-            idx=linenumber-skipped_lines
-            if l is None:
-                #editor.append("\n")
-                if annotation is None:
-                    annotation="<"
-                else:
-                    annotation+="\n<"
-                skipped_lines+=1
-                editor.setMarginText(idx, "~", marginTextStyle)
-            else:
-                if annotation is not None:
-                    editor.annotate(idx-1, annotation, 0)
-                    annotation=None
-                if '\0' in l or '\1' in l:
-                    editor.append(l.replace('\0+', '').replace('\0-', '').replace('\m', '').replace('\0^', '').replace('\1', ''))
-                    editor.markerAdd(idx, QsciScintilla.Circle)
-                    editor.setMarginText(idx, l[1], marginTextStyle)
-                    editor.fillIndicatorRange(idx, l.find('\0'), idx, l.rfind("\1"), 0)
-                else:
-                    editor.append(l)
 
     def __btn_action(self):
         self.right_editor.setFirstVisibleLine(self.left_editor.firstVisibleLine())
